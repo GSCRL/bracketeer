6e77d8bd4b8dde61af631b7c5051fbb2467b8cd2
Kim <MC42@users.noreply.github.com>
2025-04-06 12:18:24 -0500

    Added basic rendering of settings page.
    Still need to work on cleaning up settings r/w, maybe even using API keys to avoid accidentally changing anything.

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ from flask_socketio import SocketIO, emit, join_room, rooms
 
 from src.matches.match_results import _json_api_stub, match_results
 from src.screens.user_screens import user_screens
+from src.debug.debug import debug_pages
 from src.util.wrappers import ac_render_template
+
+from src.utils import runtime_err_warn
+
 
 logging.basicConfig(level="INFO")
 
@@ -14,14 +18,24 @@ socketio = SocketIO(app)
 
 app.register_blueprint(user_screens, url_prefix="/screens")
 app.register_blueprint(match_results, url_prefix="/matches")
-
+app.register_blueprint(debug_pages, url_prefix="/debug")
 
 @app.route("/")
 def index():
     return ac_render_template("homepage.html", title="Landing Page")
 
+@app.route("/settings", methods=("GET", "POST"))
+@runtime_err_warn
+def generateSettingsPage():
+    if request.method == "GET":
+        return ac_render_template(
+            "app_settings.html",
+        )
+
 logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
+
+app.config["SECRET_KEY"] = "secret secret key (required)!"
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=80, debug=True)
---
+++ src/debug/debug.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, Response, redirect, render_template, jsonify
+from src.matches.match_results import _json_api_stub
+
+debug_pages = Blueprint(
+    "debug", __name__, static_folder="./static", template_folder="./templates"
+)
+
+
+@debug_pages.route("/truefinals_requests")
+def _debug_requests():
+    from src.api_truefinals.cached_api import TrueFinalsAPICache
+
+    return jsonify(
+        TrueFinalsAPICache.select()
+        .order_by(TrueFinalsAPICache.last_requested)
+        .limit(100)
+        .output(load_json=True)
+        .run_sync()
+    )
+
+# This fails if there's no API keys present.
+# Need to work on better error handling / exposure.
+@debug_pages.route("/matches.json")
+def _debug_route_matches():
+    return jsonify(_json_api_stub()._matches)
